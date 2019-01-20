7ec50ec480e49f214fd9be3969247f130a4595c5
Henry Myers <henry@henrymyers.com>
2019-01-20 20:29:50 +0000

    Updates

---
+++ precache-manifest.22b167fa8d856d1a5398ffe68744211a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "a2efef56024928f8aef1ae376a6ed9ef",
+    "url": "/static/media/logo.a2efef56.png"
+  },
+  {
+    "revision": "f0dac89890064f7a47756c8698d2db54",
+    "url": "/static/media/bg.f0dac898.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "71a7ff882206e69b8147",
+    "url": "/static/js/main.71a7ff88.chunk.js"
+  },
+  {
+    "revision": "ed8c84cbbec32d54ce18",
+    "url": "/static/js/1.ed8c84cb.chunk.js"
+  },
+  {
+    "revision": "71a7ff882206e69b8147",
+    "url": "/static/css/main.6497923e.chunk.css"
+  },
+  {
+    "revision": "f0a793f7ede797cf1aa865253f5cd2f9",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.6e5c6a5cc2afbabb1fa36cbdd19e975b.js"
+  "/precache-manifest.22b167fa8d856d1a5398ffe68744211a.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/portfolio/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
