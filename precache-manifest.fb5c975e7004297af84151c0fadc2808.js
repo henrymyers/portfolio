02fd8f0052ab501b8b90cd9681007a99acb2e5bc
Henry Myers <hmyers@landr.com>
2019-01-21 10:34:54 -0500

    Updates

---
+++ precache-manifest.fb5c975e7004297af84151c0fadc2808.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "a2efef56024928f8aef1ae376a6ed9ef",
+    "url": "/static/media/logo.a2efef56.png"
+  },
+  {
+    "revision": "f0dac89890064f7a47756c8698d2db54",
+    "url": "/static/media/bg.f0dac898.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d1a88b859c9db49e7bb7",
+    "url": "/static/js/main.d1a88b85.chunk.js"
+  },
+  {
+    "revision": "ed8c84cbbec32d54ce18",
+    "url": "/static/js/1.ed8c84cb.chunk.js"
+  },
+  {
+    "revision": "d1a88b859c9db49e7bb7",
+    "url": "/static/css/main.4cc0e4c0.chunk.css"
+  },
+  {
+    "revision": "884271e2035cc387e6dc49a60725897a",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.6e5c6a5cc2afbabb1fa36cbdd19e975b.js"
+  "/precache-manifest.fb5c975e7004297af84151c0fadc2808.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/portfolio/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
